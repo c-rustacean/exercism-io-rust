ebec11384628d5f81f7e4211a1faaa5d2e399b53
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-08-10 00:37:49 +0300

    implemented clock
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/clock/src/lib.rs
+++ exercises/clock/src/lib.rs
@@ -1,15 +1,36 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    mins_in_day: i32,
+}
+
+const fn mins_in_day() -> i32 {
+    24 * 60
+}
+
+const fn mins_within_day(overflowed_minutes: i32) -> i32 {
+    ((overflowed_minutes % mins_in_day()) + mins_in_day()) % mins_in_day()
+}
+
+const fn in_minutes(hours: i32, minutes: i32) -> i32 {
+    hours * 60 + minutes
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let mins = mins_within_day(in_minutes(hours, minutes));
+
+        Clock { mins_in_day: mins }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let mins = mins_within_day(self.mins_in_day + minutes);
+
+        Clock { mins_in_day: mins }
+    }
+}
+
+impl ToString for Clock {
+    fn to_string(&self) -> String {
+        format!("{:02}:{:02}", self.mins_in_day / 60, self.mins_in_day % 60)
     }
 }
