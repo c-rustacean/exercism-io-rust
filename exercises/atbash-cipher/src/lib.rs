1a34c79a7046caab91eb601ad3e0940f33a0bd1b
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-08-10 03:19:49 +0300

    Implemented atbash-cipher
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/atbash-cipher/src/lib.rs
+++ exercises/atbash-cipher/src/lib.rs
@@ -1,9 +1,47 @@
+fn alphabet() -> String {
+    (b'a'..=b'z').map(char::from).collect::<String>()
+}
+
+fn atbash_letter(letter: char) -> char {
+    let alphabet = alphabet();
+
+    match alphabet.chars().position(|x| x == letter) {
+        Some(pos) => alphabet
+            .chars()
+            .nth(25 - pos)
+            .expect(&format!("Unexpected index {}", pos)),
+        _ => letter,
+    }
+}
+
+fn toggle(text: &str) -> String {
+    text.to_lowercase()
+        .chars()
+        .filter(|c| c.to_ascii_lowercase().is_ascii_alphanumeric())
+        .map(|c| atbash_letter(c))
+        .collect()
+}
+
+fn char_as_string(t: (usize, char)) -> String {
+    let mut r = String::new();
+
+    if (t.0 > 0) && (t.0 % 5 == 0) {
+        r.push(' ');
+    };
+    r.push(t.1);
+
+    r
+}
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    toggle(plain)
+        .char_indices()
+        .map(|x| char_as_string(x))
+        .collect::<String>()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    toggle(cipher).split_whitespace().collect::<String>()
 }
