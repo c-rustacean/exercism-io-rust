e93a11953c076f0949046cd6ad6999baa4b4a928
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-09-06 09:02:44 +0300

    WIP:anagram: Trying an inventory of the letters to identify anagrams
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/anagram/src/lib.rs
+++ exercises/anagram/src/lib.rs
@@ -1,9 +1,27 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
+// let letter_add =
+
+fn letter_inventory(word: &str) -> HashMap<char, i32> {
+    let mut letters = HashMap::new();
+
+    let scrabble = word.to_lowercase().chars().fold(letters, |mut letters, c| {
+        let occurences = letters.entry(c).or_insert(0);
+        *occurences += 1;
+
+        letters
+        }
     );
+
+    scrabble
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+
+    let scrabble = letter_inventory(word);
+
+    // let anagrams = HashSet::new();
+
+    possible_anagrams.into_iter().filter(|w| (*w != &word) && (letter_inventory(*w) == scrabble)).cloned().collect::<HashSet<&'a str>>()
 }
