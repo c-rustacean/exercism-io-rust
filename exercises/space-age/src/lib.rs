c250f7a3cdcce38f59e3192f23bb0e842de453bf
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-09-15 23:48:44 +0300

    Solve space age with trait associated consts
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/space-age/src/lib.rs
+++ exercises/space-age/src/lib.rs
@@ -2,20 +2,20 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    duration: u64,
+}
 
 impl From<u64> for Duration {
-    fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+    fn from(duration: u64) -> Self {
+        Duration { duration }
     }
 }
 
 pub trait Planet {
+    const ORBITAL: f64;
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        d.duration as f64 / Self::ORBITAL
     }
 }
 
@@ -28,11 +28,27 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    const ORBITAL: f64 = 31_557_600f64 * 0.240_846_7;
+}
+impl Planet for Venus {
+    const ORBITAL: f64 = 31_557_600f64 * 0.615_197_26;
+}
+impl Planet for Earth {
+    const ORBITAL: f64 = 31_557_600f64 * 1.0;
+}
+impl Planet for Mars {
+    const ORBITAL: f64 = 31_557_600f64 * 1.880_815_8;
+}
+impl Planet for Jupiter {
+    const ORBITAL: f64 = 31_557_600f64 * 11.862_615;
+}
+impl Planet for Saturn {
+    const ORBITAL: f64 = 31_557_600f64 * 29.447_498;
+}
+impl Planet for Uranus {
+    const ORBITAL: f64 = 31_557_600f64 * 84.016_846;
+}
+impl Planet for Neptune {
+    const ORBITAL: f64 = 31_557_600f64 * 164.791_320;
+}
