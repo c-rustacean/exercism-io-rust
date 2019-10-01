8b29d98eb83c95bfdcca2779f1229b51243de6f0
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-10-02 00:44:18 +0300

    atbash-cipher: Simpler atbash encoding and spacing
    As per the mentor comments, simplify atbash encoding with a pure
    mathematical function.
    
    Also use the nice idea to option<T>.into_iter() to avoid allocating on
    the stack intermediate strings or vectors, and also avoid the problem of
    having different types of iterators on the arms of the if (when
    beginning a new 5 digit block). With plain iterators the if arm would be
    std::iter::once, while the else woudl be std::iter::empty.
    
    This version of the code is my 7th version uploaded on exercism.io.
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/atbash-cipher/src/lib.rs
+++ exercises/atbash-cipher/src/lib.rs
@@ -1,47 +1,36 @@
-// fn alphabet() -> String {
-//     (b'a'..=b'z').map(char::from).collect::<String>()
-// }
-
-const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
-
-fn atbash_letter(letter: char) -> char {
-    // let alphabet = alphabet();
-
-    match ALPHABET.chars().position(|x| x == letter) {
-        Some(pos) => ALPHABET
-
-    // match alphabet.chars().position(|x| x == letter) {
-    //     Some(pos) => alphabet
-            .chars()
-            .nth(25 - pos)
-            .unwrap_or_else(|| panic!("Unexpected index {}", pos)),
-        _ => letter,
+fn atbash(letter: u8) -> char {
+    if letter.is_ascii_digit() {
+        letter
+    } else {
+        (b'z' - letter + b'a')
     }
+    .into()
 }
 
 fn toggle(text: &str) -> impl Iterator<Item = char> + '_ {
     text.chars()
-        .map(|c| c.to_ascii_lowercase())
+        .map(|c| (c as u8).to_ascii_lowercase())
         .filter(|c| c.is_ascii_alphanumeric())
-        .map(atbash_letter)
+        .map(atbash)
 }
 
-fn char_as_string(t: (usize, char)) -> String {
-    let mut r = String::with_capacity(2); // be ready for a char and a space
-
-    if (t.0 > 0) && (t.0 % 5 == 0) {
-        r.push(' ');
-    };
-    r.push(t.1);
-
-    r
+fn position_begins_chunk(position: usize) -> bool {
+    position > 0 && position % 5 == 0
 }
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
     toggle(plain)
         .enumerate()
-        .map(char_as_string)
+        .flat_map(|(p, c)| {
+            if position_begins_chunk(p) {
+                Some(' ')
+            } else {
+                None
+            }
+            .into_iter()
+            .chain(std::iter::once(c))
+        })
         .collect::<String>()
 }
 
@@ -49,3 +38,24 @@ pub fn encode(plain: &str) -> String {
 pub fn decode(cipher: &str) -> String {
     toggle(cipher).collect::<String>()
 }
+
+// fn alphabet() -> String {
+//     (b'a'..=b'z').map(char::from).collect::<String>()
+// }
+
+// fn atbash(letter: char) -> char {
+//     let alphabet = alphabet();
+
+//     let atbashed = alphabet
+//         .chars()
+//         .zip(alphabet.chars().rev())
+//         .filter(|(direct, _reversed)| *direct == letter)
+//         .map(|(_d, r)| r)
+//         .collect::<Vec<char>>();
+
+//     if atbashed.is_empty() {
+//         letter
+//     } else {
+//         atbashed[0]
+//     }
+// }
