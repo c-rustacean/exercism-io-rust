d1ea9d528d1054799d84717a845b6310d7a6fb2b
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-10-12 02:08:22 +0300

    Solved sublist
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/sublist/src/lib.rs
+++ exercises/sublist/src/lib.rs
@@ -6,6 +6,42 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+fn sublist_of<T: PartialEq>(short_list: &[T], long_list: &[T]) -> Comparison {
+    let short_len = short_list.len();
+    let last_possible_index = long_list.len() - short_len;
+
+    for (pos, item) in long_list[..=last_possible_index].iter().enumerate() {
+        if short_list[0] == *item && short_list == &long_list[pos..(pos + short_len)] {
+            return Comparison::Sublist;
+        }
+    }
+
+    Comparison::Unequal
+}
+
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let l1 = first_list.len();
+    let l2 = second_list.len();
+
+    match (l1, l2) {
+        (0, 0) => Comparison::Equal,
+        (0, _) => Comparison::Sublist,
+        (_, 0) => Comparison::Superlist,
+        (_, _) if l1 == l2 => {
+            if first_list == second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
+        (_, _) if l1 < l2 => sublist_of(first_list, second_list),
+        (_, _) => {
+            let r = sublist_of(second_list, first_list);
+            if r == Comparison::Sublist {
+                Comparison::Superlist
+            } else {
+                r
+            }
+        }
+    }
 }
