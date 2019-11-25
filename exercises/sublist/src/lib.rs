840175a79f1481b6339f415cd4dbdbb921a8b198
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-11-26 01:28:44 +0200

    sublist: Add my final version of the exercise, after useful input from mentor
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/sublist/src/lib.rs
+++ exercises/sublist/src/lib.rs
@@ -6,42 +6,23 @@ pub enum Comparison {
     Unequal,
 }
 
-fn sublist_of<T: PartialEq>(short_list: &[T], long_list: &[T]) -> Comparison {
+fn sublist_of<T: PartialEq>(short_list: &[T], long_list: &[T]) -> bool {
     let short_len = short_list.len();
-    let last_possible_index = long_list.len() - short_len;
 
-    for (pos, item) in long_list[..=last_possible_index].iter().enumerate() {
-        if short_list[0] == *item && short_list == &long_list[pos..(pos + short_len)] {
-            return Comparison::Sublist;
-        }
-    }
-
-    Comparison::Unequal
+    short_len == 0 || long_list.windows(short_len).any(|w| w == short_list)
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    use std::cmp::Ordering;
+    use Comparison::*;
+
     let l1 = first_list.len();
     let l2 = second_list.len();
 
-    match (l1, l2) {
-        (0, 0) => Comparison::Equal,
-        (0, _) => Comparison::Sublist,
-        (_, 0) => Comparison::Superlist,
-        (_, _) if l1 == l2 => {
-            if first_list == second_list {
-                Comparison::Equal
-            } else {
-                Comparison::Unequal
-            }
-        }
-        (_, _) if l1 < l2 => sublist_of(first_list, second_list),
-        (_, _) => {
-            let r = sublist_of(second_list, first_list);
-            if r == Comparison::Sublist {
-                Comparison::Superlist
-            } else {
-                r
-            }
-        }
+    match l1.cmp(&l2) {
+        Ordering::Equal if first_list == second_list => Equal,
+        Ordering::Less if sublist_of(first_list, second_list) => Sublist,
+        Ordering::Greater if sublist_of(second_list, first_list) => Superlist,
+        _ => Unequal,
     }
 }
