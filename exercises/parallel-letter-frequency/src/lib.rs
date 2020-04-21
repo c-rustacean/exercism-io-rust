57eae00384fbe2ca19cf77275e3309cf3e9e4cb3
Eddy Petrișor <eddy.petrisor@gmail.com>
2020-04-22 00:08:55 +0300

    parallel-letter-frequency: Ugly parallel implementation
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- exercises/parallel-letter-frequency/src/lib.rs
+++ exercises/parallel-letter-frequency/src/lib.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::sync::mpsc::channel;
+use std::thread;
 
 fn str_freq(s: &str) -> HashMap<char, usize> {
     s.to_lowercase()
@@ -10,10 +12,59 @@ fn str_freq(s: &str) -> HashMap<char, usize> {
         })
 }
 
+#[cfg(single_thread)]
 fn chunk_freq(input: &[&str]) -> HashMap<char, usize> {
     str_freq(&input.concat())
 }
 
+#[cfg(single_thread)]
 pub fn frequency(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
     chunk_freq(input)
+}
+
+pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    let mut handles = Vec::with_capacity(worker_count);
+    let mut hashes: HashMap<char, usize> = HashMap::new();
+
+    let input = input.concat();
+
+    let len = input.chars().count();
+    let chunk_size = (len + worker_count - 1) / worker_count;
+
+    if chunk_size == 0 {
+        return HashMap::new();
+    } else if chunk_size < 1024usize {
+        return str_freq(&input);
+    }
+
+    let mut chunks: Vec<String> = input
+        .chars()
+        .collect::<Vec<char>>()
+        .chunks(chunk_size)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect();
+
+    let (tx, rx) = channel();
+    let worker_count = chunks.len();
+    for _ in 0..worker_count {
+        let tx = tx.clone();
+        let ch = chunks.pop().unwrap();
+        handles.push(thread::spawn(move || {
+            tx.send(str_freq(ch.as_str())).unwrap();
+        }));
+    }
+
+    for _ in 0..worker_count {
+        let hash_t = rx.recv().unwrap();
+        for (k, v) in hash_t.iter() {
+            let counter = hashes.entry(*k).or_insert(0);
+            *counter += v;
+        }
+    }
+
+    for h in handles {
+        h.join().expect("Could not join thread");
+    }
+
+    hashes
 }
